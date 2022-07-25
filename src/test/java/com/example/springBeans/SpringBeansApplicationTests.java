5acaa274d546f65ea18bf964d47bbf7afeef6633
vivek-prime <vivek.400428@gmail.com>
2022-07-25 19:03:40 +0530

    first commit

---
+++ src/test/java/com/example/springBeans/SpringBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
